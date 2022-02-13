b22f9b000e3638d3404050eb882e0eea16561f90
Krishna Joshi <krishnajoshi399@gmail>
2022-02-13 13:43:20 +0530

    added Sieve of Eratosthenes

---
+++ BASICS/Maths/SieveOfEratosthenes.java
@@ -0,0 +1,32 @@
+/*
+
+print prime no's till 'n' = 20
+2 3 5 7 11 13 17 19
+
+Time complexity of below program = O(n)
+Space complexity of below program = O(n)
+
+*/
+
+import java.util.Scanner;
+
+class SieveOfEratosthenes{
+    public static void sieveOfEratosthenes(int n){
+
+        boolean[] prime = new boolean[n+1];
+
+        for(int i=2;i*i<=n;i++){
+            for(int j=i*2;j<=n;j+=i)
+                prime[j] = true;
+        }
+        for(int i=2;i<=n;i++)
+            if(!prime[i])
+            System.out.print(i+" ");
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter x and y values for range: ");
+        int n = sc.nextInt();
+        sieveOfEratosthenes(n);
+    }
+}
